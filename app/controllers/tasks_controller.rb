8139a1a9581d66ee9863afa8b6f9c7f00852d833
Milo Delgado <milo@citrusme.com>
2024-01-10 16:53:47 -0600

    remove solutions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
-
   # GET /tasks
   def index
-    @tasks = Task.all
   end
 
   # GET /tasks/1
@@ -12,7 +9,6 @@ class TasksController < ApplicationController
 
   # GET /tasks/new
   def new
-    @task = Task.new
   end
 
   # GET /tasks/1/edit
@@ -21,40 +17,14 @@ class TasksController < ApplicationController
 
   # POST /tasks
   def create
-    @task = Task.new(task_params)
-
-    if @task.save
-      redirect_to @task, notice: "Task was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
   end
 
   # PATCH/PUT /tasks/1
   def update
-    if @task.update(task_params)
-      redirect_to @task, notice: "Task was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
   end
 
   # DELETE /tasks/1
   def destroy
-    @task.destroy
-    redirect_to tasks_url, notice: "Task was successfully destroyed."
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def task_params
-    params.require(:task).permit(:title, :completed)
   end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,2 @@
 class Task < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 3 }
-  validates :completed, inclusion: { in: [true, false] }
 end
