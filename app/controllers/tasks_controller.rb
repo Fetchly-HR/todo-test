40ca1d9353b983e6a9a3ef1aea5d3fc482eef59a
Milo Delgado <milo@citrusme.com>
2024-01-12 13:50:32 -0600

    add back tasks controller so upload works

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+  # GET /tasks
+  def index
+  end
+
+  # GET /tasks/1
+  def show
+  end
+
+  # GET /tasks/new
+  def new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks
+  def create
+  end
+
+  # PATCH/PUT /tasks/1
+  def update
+  end
+
+  # DELETE /tasks/1
+  def destroy
+  end
+
+end
